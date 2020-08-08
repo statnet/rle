1c8f6bad51df44f8343d5aaa304466b86e728211
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-08-08 10:59:54 +1000

    Added copyright headers and a custom script to update them.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
+#  File R/zzz.R in package rle, currently hosted at https://github.com/statnet/rle .
+#
+#  This software is distributed under the GNU General Public License Version 3 or later.
+#  A copy of this license may be found at https://www.gnu.org/licenses/gpl-3.0.en.html .
+#
+#  Copyright 2017-2020 Pavel N. Krivitsky and others (see inst/COPYRIGHT).
+#######################################################################
 #' @useDynLib rle
 
 .onUnload <- function(libpath){
--- src/rle_utils.c
+++ src/rle_utils.c
@@ -1,11 +1,9 @@
-/*  File src/rle_utils.c in package statnet.common, part of the Statnet suite
- *  of packages for network analysis, https://statnet.org .
+/*  File src/rle_utils.c in package rle, currently hosted at https://github.com/statnet/rle .
  *
- *  This software is distributed under the GPL-3 license.  It is free,
- *  open source, and has the attribution requirements (GPL Section 7) at
- *  https://statnet.org/attribution
+ *  This software is distributed under the GNU General Public License Version 3 or later.
+ *  A copy of this license may be found at https://www.gnu.org/licenses/gpl-3.0.en.html .
  *
- *  Copyright 2007-2019 Statnet Commons
+ *  Copyright 2017-2020 Pavel N. Krivitsky and others (see inst/COPYRIGHT).
  */
 #include<R.h>
 #include<Rmath.h>
